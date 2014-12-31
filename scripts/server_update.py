c172dc53d9c38ed95b6192a9edc1d44f3cc4eec7
Leonardo José Consoni <consoni_2519@hotmail.com>
2014-12-31 17:29:18 -0200

    Visual Studio project refactoring [2]

--- scripts/server_update.py
+++ scripts/server_update.py
@@ -58,7 +58,7 @@ class AxisServer:
    axisDataClients = {}
    
    # Variáveis para medição de desempenho (benchmark)
-   logNSamples = 100
+   logNSamples = 10
    logSamplesCount = 0
    messageArrivalTimes = []
    
@@ -85,7 +85,7 @@ class AxisServer:
       for client in self.dataClients:
          message = receiveMessage( client )
          if message is not None:
-            print( 'Axis Server receive: ' + message )
+            # print( 'Axis Server receive: ' + message )
             clientInfo = message.split(':')
             if ( len(clientInfo) - 1 ) % 3 == 0:
                if clientInfo[0] == 'Axis Data':
@@ -95,10 +95,11 @@ class AxisServer:
                      
                   # Teste Latência
                   messageIndex = int(clientInfo[2])                                                     # benchmark
-                  if self.logSamplesCount < self.logNSamples:                                           # benchmark
+                  if self.logSamplesCount < self.logNSamples and messageIndex < self.logNSamples - 1:   # benchmark
                      if self.messageArrivalTimes[ messageIndex ] == 0: self.logSamplesCount += 1        # benchmark  
                      self.messageArrivalTimes[ messageIndex ] = int(execTime() * 1000)                  # benchmark
-                     
+                     print( 'Message ' + str(messageIndex) + ' received at ' + str(self.messageArrivalTimes[ messageIndex ]) )
+                     print( str(self.logSamplesCount) + ' messages received' )                          # benchmark
                   if self.logSamplesCount == self.logNSamples:                                          # benchmark
                      print( 'Saving Log' )                                                              # benchmark
                      messageArrivalLog = open( 'data/message_arrival_times.txt', 'w' )                  # benchmark
