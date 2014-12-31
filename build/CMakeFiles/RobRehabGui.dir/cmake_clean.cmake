ad768f3371e4e662a6fecd3fde8924e30def07e6
Bitiquinho <consoni_2519@hotmail.com>
2014-12-31 12:32:16 -0200

    Add missing files

---
+++ build/CMakeFiles/RobRehabGui.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RobRehabGui.dir/src/gui_main.cpp.o"
+  "../RobRehabGui.pdb"
+  "../RobRehabGui"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RobRehabGui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
