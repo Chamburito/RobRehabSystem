999d720c307a4bc76b223eabafcd39ea57a02a70
Bitiquinho <consoni_2519@hotmail.com>
2016-08-23 14:51:16 -0300

    Xenomai RT main placeholder. Simple actuator control plugin.

--- src/actuator_control/force_pi_control.c
+++ src/actuator_control/force_pi_control.c
@@ -4,10 +4,6 @@
 
 typedef struct _ControlData
 {
-  double positionErrorSum, positionSetpointSum;
-  double velocitySetpoint;
-  double forceError[ 2 ];
-  double controlError;
   ControlVariablesList outputsList;
 }
 ControlData;
@@ -23,43 +19,15 @@ Controller InitController( void )
 }
 
 double* RunControlStep( Controller controller, double* measuresList, double* setpointsList, double* ref_error )
-{
-  const double K_P = 370;//1.6527; // 370 * ( F_in_max / V_out_max )
-  const double K_I = 3.5;//0.0156; // 3.5 * ( F_in_max / V_out_max )
+{ 
+  if( controller == NULL ) return NULL;
   
   ControlData* controlData = (ControlData*) controller;
-  
-  controlData->outputsList[ CONTROL_POSITION ] = setpointsList[ CONTROL_POSITION ];
-  
-  double positionError = measuresList[ CONTROL_POSITION ] - setpointsList[ CONTROL_POSITION ];
-  controlData->positionErrorSum += CONTROL_PASS_INTERVAL * positionError * positionError;
-  controlData->positionSetpointSum += CONTROL_PASS_INTERVAL * setpointsList[ CONTROL_POSITION ] * setpointsList[ CONTROL_POSITION ];
-  
-  if( ref_error != NULL )
-  {
-    if( *ref_error == 0.0 )
-    {
-      controlData->controlError = ( controlData->positionSetpointSum > 0.0 ) ? controlData->positionErrorSum / controlData->positionSetpointSum : 1.0;
-      if( controlData->controlError > 1.0 ) controlData->controlError = 1.0;
-    
-      controlData->positionErrorSum = 0.0;
-      controlData->positionSetpointSum = 0.0;
-    }
-    
-    *ref_error = positionError / setpointsList[ CONTROL_POSITION ];
-  }
-  
-  double forceSetpoint = controlData->controlError * setpointsList[ CONTROL_FORCE ];
-  controlData->outputsList[ CONTROL_FORCE ] = forceSetpoint;
 
-  controlData->forceError[ 0 ] = forceSetpoint - measuresList[ CONTROL_FORCE ];
-  
-  controlData->velocitySetpoint += K_P * ( controlData->forceError[ 0 ] - controlData->forceError[ 1 ] ) + K_I * CONTROL_PASS_INTERVAL * controlData->forceError[ 0 ];
-  controlData->outputsList[ CONTROL_VELOCITY ] = controlData->velocitySetpoint;
-  
-  //velocitySetpoint[0] = 0.9822 * velocitySetpoint[1] + 0.01407 * velocitySetpoint[2] + 338.6 * forceError[1] - 337.4 * forceError[2]; //5ms
-  
-  controlData->forceError[ 1 ] = controlData->forceError[ 0 ];
+  controlData->outputsList[ CONTROL_POSITION ] = setpointsList[ CONTROL_POSITION ];
+  controlData->outputsList[ CONTROL_VELOCITY ] = setpointsList[ CONTROL_VELOCITY ];
+  controlData->outputsList[ CONTROL_FORCE ] = setpointsList[ CONTROL_FORCE ];
+  controlData->outputsList[ CONTROL_ACCELERATION ] = setpointsList[ CONTROL_ACCELERATION ];
 
   return (double*) controlData->outputsList;
 }
---
+++ src/robrehab_system_xenomai.c
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  Copyright (c) 2016 Leonardo José Consoni                                  //
+//                                                                            //
+//  This file is part of RobRehabSystem.                                      //
+//                                                                            //
+//  RobRehabSystem is free software: you can redistribute it and/or modify    //
+//  it under the terms of the GNU Lesser General Public License as published  //
+//  by the Free Software Foundation, either version 3 of the License, or      //
+//  (at your option) any later version.                                       //
+//                                                                            //
+//  RobRehabSystem is distributed in the hope that it will be useful,         //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              //
+//  GNU Lesser General Public License for more details.                       //
+//                                                                            //
+//  You should have received a copy of the GNU Lesser General Public License  //
+//  along with RobRehabSystem. If not, see <http://www.gnu.org/licenses/>.    //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include <native/task.h>
+#include <native/queue.h>
+#include <native/intr.h>
+#include <native/pipe.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <signal.h>
+
+#include "debug/async_debug.h"
+
+#include "robrehab_subsystem.h"
+
+
+static volatile bool isRunning = true;
+
+void HandleExit( int dummyData )
+{
+  DEBUG_PRINT( "received exit signal: %d", dummyData );
+  isRunning = false;
+}
+
+int main( int argc, char* argv[] )
+{
+  const struct timespec UPDATE_TIMESPEC = { .tv_nsec = 1000000 * UPDATE_INTERVAL_MS };
+  
+  time_t rawTime;
+  time( &rawTime );
+  DEBUG_PRINT( "starting control program at time: %s", ctime( &rawTime ) );
+  
+  signal( SIGINT, HandleExit );
+  
+  if( argc > 1 )
+  {
+    if( SubSystem.Init( argv[ 1 ], NULL, NULL ) != -1 )
+    {
+      while( isRunning ) // Check for program termination conditions
+      {
+        SubSystem.Update();
+      
+        nanosleep( &UPDATE_TIMESPEC, NULL ); // Sleep to give the desired loop rate.
+      }
+    }
+  }
+  
+  time( &rawTime );
+  DEBUG_PRINT( "ending control program at time: %s", ctime( &rawTime ) );
+
+  SubSystem.End();
+  
+  exit( 0 );
+}
