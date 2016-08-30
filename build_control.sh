bff2606896826a73ffc24a91319f157c4b701e25
Bitiquinho <consoni_2519@hotmail.com>
2016-08-30 12:31:48 -0300

    Improve custom build scripts (exernal flags and -fPIC option for plugins compilation).

--- build_control.sh
+++ build_control.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-gcc -std=gnu99 -DROBREHAB_CONTROL -D__USE_POSIX199309 -D_DEFAULT_SOURCE=__STRICT_ANSI__ \
+gcc -std=gnu99 $@ -DROBREHAB_CONTROL -D__USE_POSIX199309 -D_DEFAULT_SOURCE=__STRICT_ANSI__ \
     -D_SVID_SOURCE -DDEBUG -Isrc -Isrc/actuator_control/ -Isrc/robot_control \
     -Isrc/data_io -Isrc/signal_io -Isrc/time -Isrc/threads -Isrc/shared_memory \
     src/robrehab_system.c src/robrehab_control.c src/threads/thread_safe_data.c \
     src/shm_control.c src/shared_memory/shm_unix.c src/threads/threads_unix.c src/debug/data_logging.c \
     src/time/timing_unix.c src/configuration.c src/motors.c src/curve_interpolation.c \
     src/kalman_filters.c src/matrices_blas.c src/actuators.c src/robots.c src/sensors.c \
-    src/signal_processing.c -o RobRehabControl -ldl -lrt -lpthread -lblas -llapack
+    src/signal_processing.c -o RobRehabControl -lm -ldl -lrt -lpthread -lblas -llapack
--- build_server.sh
+++ build_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc -std=gnu99 -DROBREHAB_SERVER -D__USE_POSIX199309 -D_DEFAULT_SOURCE=__STRICT_ANSI__ \
+gcc -std=gnu99 $@ -DROBREHAB_SERVER -D__USE_POSIX199309 -D_DEFAULT_SOURCE=__STRICT_ANSI__ \
     -D_SVID_SOURCE -DIP_NETWORK_LEGACY -DDEBUG -Isrc -Isrc/ip_network/ src/robrehab_system.c \
     src/robrehab_network.c src/ip_network/ip_network.c src/ip_network/async_ip_network.c \
     src/threads/thread_safe_data.c src/shm_control.c src/shared_memory/shm_unix.c \
